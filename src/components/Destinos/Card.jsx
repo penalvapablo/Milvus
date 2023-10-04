d0198e71b66809303fd8f5b4fd52d5a31b44e748
Pablo Penalva <penalva.pablo@gmail.com>
2023-10-04 16:34:44 -0300

    destinos grid fixed

--- src/components/Destinos/Card.jsx
+++ src/components/Destinos/Card.jsx
@@ -1,8 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleXmark,
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 
 const Card = ({
+  destinos,
   destino,
   titulo,
   descripcion,
@@ -10,7 +15,7 @@ const Card = ({
   cta,
   idioma,
 }) => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(undefined);
   const [mobileColor, setMobileColor] = useState('one');
   const [desktopColor, setDesktopColor] = useState('one');
 
@@ -26,14 +31,30 @@ const Card = ({
   }, []);
 
   const modalFn = () => {
-    setModal(!modal);
-    if (typeof window !== 'undefined') {
-      !modal
-        ? (document.body.style.overflow = 'hidden')
-        : (document.body.style.overflow = 'auto');
+    if (modal !== undefined) {
+      setModal(undefined);
+      if (typeof window !== 'undefined') {
+        document.body.style.overflow = 'auto';
+      }
+    } else {
+      setModal(id);
+      if (typeof window !== 'undefined') {
+        document.body.style.overflow = 'hidden';
+      }
     }
   };
 
+  function arrowLeft() {
+    if (modal !== 1) {
+      setModal(modal - 1);
+    }
+  }
+
+  function arrowRight() {
+    if (modal !== 12) {
+      setModal(modal + 1);
+    }
+  }
   return (
     <>
       {modal && (
@@ -41,7 +62,8 @@ const Card = ({
           <div
             onClick={modalFn}
             className="fixed left-1/2 top-1/2 z-10 h-screen w-screen translate-x-[-50%] translate-y-[-50%]  bg-black/50"></div>
-          <div className="fixed left-1/2  top-1/2 z-50 h-fit max-h-[600px] w-[90%]  max-w-[800px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-two  xl:h-[600px] xl:w-[600px] xl:max-w-none min-tall-mobile:max-h-[600px] ">
+          <h1>{}</h1>
+          <div className="fixed left-1/2  top-1/2 z-50 h-fit max-h-[600px] w-[85%]  max-w-[800px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-two  xl:h-[600px] xl:w-[600px] xl:max-w-none min-tall-mobile:max-h-[600px] ">
             <button
               onClick={modalFn}
               id="close-modal"
@@ -51,27 +73,55 @@ const Card = ({
                 className="absolute right-[-12px] top-[-12px] z-10 h-8 rounded-full bg-two text-four "
               />
             </button>
+            <button
+              id="left arrow"
+              aria-label="left arrow"
+              onClick={arrowLeft}>
+              {modal !== 1 && (
+                <FontAwesomeIcon
+                  icon={faCircleArrowLeft}
+                  className="absolute left-[-15px] bottom-[50%] z-10 h-8 rounded-full bg-two text-four "
+                />
+              )}
+            </button>
+            <button
+              onClick={arrowRight}
+              id="right arrow"
+              aria-label="right arrow">
+              {modal !== 12 && (
+                <FontAwesomeIcon
+                  icon={faCircleArrowRight}
+                  className="absolute right-[-15px] bottom-[50%] z-10 h-8 rounded-full bg-two text-four "
+                />
+              )}
+            </button>
             <picture>
               <source
                 type="image/webp"
-                srcSet={`/destinos/${destino}-modal.webp`}
+                srcSet={`/destinos/${
+                  destinos[modal - 1].destino
+                }-modal.webp`}
               />
               <img
-                src={`/destinos/${destino}-modal.jpg`}
+                src={`/destinos/${
+                  destinos[modal - 1].destino
+                }-modal.jpg`}
                 alt={destino}
                 loading="lazy"
                 className="absolute top-0 -z-10 hidden rounded-xl xl:block"
               />
             </picture>
             <h3 className="mx-auto max-w-[90%] text-center text-5xl font-semibold capitalize text-one xl:hidden">
-              {titulo}
+              {destinos[modal - 1].titulo}
             </h3>
 
-            <p className="mx-auto mt-8 max-h-[100px] w-[90%] scroll-ml-1 overflow-y-scroll text-center  text-base font-semibold tracking-wide text-white xl:hidden min-tall-mobile3:max-h-[300px] min-tall-mobile2:max-h-[300px]">
+            <p className="mx-auto mt-8 max-h-[100px] w-[85%] scroll-ml-1 overflow-y-scroll text-center  text-base font-semibold tracking-wide text-white xl:hidden min-tall-mobile3:max-h-[300px] min-tall-mobile2:max-h-[300px]">
               {idioma === 'ar' ? (
-                <bdi dir="rtl">{descripcion}</bdi>
+                <bdi dir="rtl">
+                  {destinos[modal - 1].descripcion}
+                </bdi>
               ) : (
-                <>{descripcion}</>
+                <>{destinos[modal - 1].descripcion}</>
               )}
             </p>
 
@@ -81,7 +131,7 @@ const Card = ({
                   ? '962799757654'
                   : '5215554094418'
               }`}
-              className="mx-auto mt-8 mb-7 block w-[95%] max-w-[300px] transform rounded-full bg-four px-[3.75rem] py-2 text-center text-xl font-bold text-two duration-200 hover:bg-one xl:hidden"
+              className="mx-auto mt-8 mb-7 block w-[90%] max-w-[300px] transform rounded-full bg-four px-[3.75rem] py-2 text-center text-xl font-bold text-two duration-200 hover:bg-one xl:hidden"
               rel="noreferrer"
               target="_blank">
               {cta}
@@ -89,14 +139,16 @@ const Card = ({
             <div className="absolute bottom-0 hidden h-full w-full rounded-xl bg-black/70 xl:block">
               <h3
                 className={`mt-8 hidden text-center text-6xl font-semibold capitalize text-white xl:block`}>
-                {titulo}
+                {destinos[modal - 1].titulo}
               </h3>
               <div className="absolute bottom-0 mb-7 flex flex-col items-center gap-7">
                 <p className="w-[90%]  text-center text-base font-semibold tracking-wide text-white">
                   {idioma === 'ar' ? (
-                    <bdi dir="rtl">{descripcion}</bdi>
+                    <bdi dir="rtl">
+                      {destinos[modal - 1].descripcion}
+                    </bdi>
                   ) : (
-                    <>{descripcion}</>
+                    <>{destinos[modal - 1].descripcion}</>
                   )}
                 </p>
                 <a
@@ -105,7 +157,7 @@ const Card = ({
                       ? '962799757654'
                       : '5215554094418'
                   }`}
-                  class="block w-[300px] transform rounded-full bg-four px-[3.75rem] py-2 text-center text-xl font-bold text-two duration-200 hover:bg-one"
+                  className="block w-[300px] transform rounded-full bg-four px-[3.75rem] py-2 text-center text-xl font-bold text-two duration-200 hover:bg-one"
                   rel="noreferrer"
                   target="_blank">
                   {cta}
@@ -119,7 +171,7 @@ const Card = ({
       <button
         id={destino}
         aria-label={destino}
-        className="relative"
+        className="relative scale-100 transform transition-transform hover:scale-105"
         onClick={modalFn}>
         <picture>
           <source
@@ -130,7 +182,7 @@ const Card = ({
             src={`/destinos/${destino}-mobile.jpg`}
             alt={destino}
             loading="lazy"
-            className="rounded-xl xl:hidden"
+            className="rounded-xl lg:hidden"
           />
         </picture>
         <picture>
@@ -142,19 +194,15 @@ const Card = ({
             src={`/destinos/${destino}-desktop.jpg`}
             alt={destino}
             loading="lazy"
-            className="hidden rounded-xl xl:block"
+            className="hidden rounded-xl lg:block"
           />
         </picture>
         <h3
-          className={`absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] text-${
-            idioma === 'ar' ? '4xl' : '5xl'
-          } font-semibold text-${mobileColor} w-[95%] xl:top-[30px] xl:hidden xl:translate-y-0`}>
+          className={`absolute left-1/2 top-1/2 w-full translate-x-[-50%] translate-y-[-50%] px-2  text-4xl font-semibold lg:hidden xl:top-[30px] text-${mobileColor}`}>
           {titulo}
         </h3>
         <h3
-          className={`absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] 
-          text-${idioma === 'ar' ? '4xl' : '5xl'}
-          font-semibold text-${desktopColor} hidden w-full xl:top-[30px] xl:block xl:translate-y-0 `}>
+          className={`absolute  hidden w-full px-2 text-4xl   font-semibold lg:top-[20px] lg:block text-${desktopColor} `}>
           {titulo}
         </h3>
       </button>
--- src/components/Destinos/Grid.jsx
+++ src/components/Destinos/Grid.jsx
@@ -3,14 +3,15 @@ import Card from './Card';
 const Grid = ({ textos, idioma }) => {
   const { destinos } = textos;
   return (
-    <ul className=" mx-auto grid  grid-cols-1 justify-items-center gap-6 md:w-[644px] md:grid-cols-2 xl:w-[978px] xl:grid-cols-3 2xl:w-[1312px] 2xl:grid-cols-4">
+    <ul className=" lg:w[800px] mx-auto  grid grid-cols-1 items-center justify-items-center gap-4 md:w-[644px] md:grid-cols-2 lg:w-[978px] lg:grid-cols-4">
       {destinos.map((destino) => {
         let id = destinos.indexOf(destino) + 1;
         return (
           <li
-            className="list-none"
+            className="flex list-none"
             key={destino.destino}>
             <Card
+              destinos={destinos}
               id={id}
               cta={textos.cta}
               destino={destino.destino}
